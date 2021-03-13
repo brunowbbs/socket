543dd8ae41e376cd1e23f5d841f38d4018fe60c5
Wesley Bruno <engwesleybruno@gmail.com>
2021-03-13 15:54:04 -0300

    first commit

---
+++ public/main.js
@@ -0,0 +1,83 @@
+const socket = io();
+let username = '';
+let userList = [];
+
+let loginPage = document.querySelector('#loginPage');
+let chatPage = document.querySelector('#chatPage');
+
+let loginInput = document.querySelector('#loginNameInput');
+let textInput = document.querySelector('#chatTextInput');
+
+
+loginPage.style.display = 'flex';
+chatPage.style.display = 'none';
+
+
+const renderUserList = () => {
+  let ul = document.querySelector('.userList');
+  ul.innerHTML = '';
+
+  userList.forEach(i => {
+    ul.innerHTML += '<li>' + i + '</i>'
+  })
+}
+
+const addMessage = (type, user, msg) => {
+  let ul = document.querySelector('.chatList');
+
+  switch (type) {
+    case 'status':
+      ul.innerHTML += '<li class="m-status">' + msg + '</li>';
+      break;
+    case 'msg':
+      ul.innerHTML += '<li class="m-txt"><span>' + user + '</span> ' + msg + '</li>';
+      break;
+  }
+}
+
+loginInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    let name = loginInput.value.trim();
+    if (name !== '') {
+      username = name;
+      document.title = 'Chat (' + username + ')';
+      socket.emit('join-request', username);
+    }
+  }
+});
+
+socket.on('user-ok', (list) => {
+  loginPage.style.display = 'none';
+  chatPage.style.display = 'flex';
+  textInput.focus();
+  addMessage('status', null, 'Conectado');
+  userList = list;
+  renderUserList();
+});
+
+
+socket.on('list-update', data => {
+  if (data.joined) {
+    addMessage('status', null, data.joined + " entrou no chat.");
+  }
+  if (data.left) {
+    addMessage('status', null, data.joined + " saiu no chat.");
+  }
+  userList = data.list;
+  renderUserList();
+})
+
+textInput.addEventListener('keyup', (e) => {
+  if (e.keyCode === 13) {
+    let txt = textInput.value.trim();
+    textInput.value = '';
+    if (txt !== '') {
+      addMessage('msg', username, txt)
+      socket.emit('send-msg', txt);
+    }
+  }
+});
+
+socket.on('show-msg', (data) => {
+  addMessage('msg', data.username, data.message)
+})
